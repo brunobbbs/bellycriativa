b48b56410ee7a1aecf5f525bb6c113466205c05c
Bruno Barbosa <bsbruno1@gmail.com>
2016-01-08 01:38:27 -0200

    add sys.path to project path works correctly on webfaction

--- bellycriativa/wsgi.py
+++ bellycriativa/wsgi.py
@@ -11,6 +11,9 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
+import sys
+
+sys.path.insert(0, '/home/brunobbbs/webapps/bellycriativa_site/bellycriativa')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("bellycriativa"))
